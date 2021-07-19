6ab1098bb8cb273236be8e6a32d0c89f99256c0d
Shiva Ganapathy <shivastem@gmail.com>
2021-07-18 21:10:46 -0400

    created table to display elements

--- inventory/src/components/AddItem.js
+++ inventory/src/components/AddItem.js
@@ -23,16 +23,16 @@ function AddItem(props){
             
             <div className="row">
                 <label htmlFor="name-field"> Name: </label>
-                <input id="name-field" type="text" value={name} onChange={(e)=>setname(e.target.value)}/>
+                <input className="form-control" id="name-field" type="text" value={name} onChange={(e)=>setname(e.target.value)}/>
                 <label htmlFor="price-field"> Price: </label>
-                <input id="price-field" type="number" value={price} onChange={(e)=>setprice(e.target.value)}/>
+                <input className="form-control" id="price-field" type="number" value={price} onChange={(e)=>setprice(e.target.value)}/>
                 <label htmlFor="type-field"> Type: </label>
-                <input id="type-field" type="text" value={type} onChange={(e)=>settype(e.target.value)}/>
+                <input className="form-control" id="type-field" type="text" value={type} onChange={(e)=>settype(e.target.value)}/>
                 <label htmlFor="brand-field"> Brand: </label>
-                <input id="brand-field" type="text" value={brand} onChange={(e)=>setbrand(e.target.value)}/>
+                <input className="form-control" id="brand-field" type="text" value={brand} onChange={(e)=>setbrand(e.target.value)}/>
             </div>
-            <div className="row">
-            <button type="button" onClick={addItemButtonPressed}> Add Item </button>
+            <div className="row mt-3">
+            <button className="btn btn-primary" type="button" onClick={addItemButtonPressed}> Add Item </button>
             </div>
             
 
