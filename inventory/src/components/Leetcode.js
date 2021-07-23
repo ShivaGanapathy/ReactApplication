477b27bc487891de84d25c12e46eb672ca702ff1
Shiva Ganapathy <shivastem@gmail.com>
2021-07-23 00:59:37 -0400

    Added color styling according to difficulty level in Leetcode.js

--- inventory/src/components/Leetcode.js
+++ inventory/src/components/Leetcode.js
@@ -2,9 +2,42 @@ import Data from "../data.json";
 
 function Leetcode(props){
 
+    // className="text-success"
+    // className="text-warning"
+    // className="font-weight-normal text-nowrap"
+    const displayItems = (item) =>{
+
+        let isEasy = item.difficulty === "Easy"
+        let isMed = item.difficulty === "Medium"
+        
+
+        return(
+            <tr>
+                <th scope="row">{item.id}</th>
+                <td>{item.title}</td>
+                <td className={isEasy ? "text-success" : isMed ? "text-warning" : "text-danger"}>{item.difficulty}</td>
+                <td>{item.blind}</td>
+            </tr>
+        )
+
+    }
+
     return (
         <div>
-            <h1>This is a test</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Problem</th>
+                        <th scope="col">Difficulty</th>
+                        <th scope="col">Included in Blind 75?</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    {Data.map(displayItems)}
+                </tbody>
+            </table>
         </div>
         
     )
